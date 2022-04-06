2452e4486bf84b624da43a7d91e6766ec5ad5dec
carlosvigan <cfernandezgarcia@hotmail.com>
2022-04-06 19:47:09 +0100

    He añadido el nombre de Fabio al mensaje

--- PruebaConFabio/src/com/carlosvigan/PruebaFabio/Principal.java
+++ PruebaConFabio/src/com/carlosvigan/PruebaFabio/Principal.java
@@ -14,7 +14,7 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola");
+		System.out.println("hola Fabio");
 	}
 
 }
