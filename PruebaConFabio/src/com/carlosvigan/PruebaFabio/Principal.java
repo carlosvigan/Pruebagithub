69ce951dd125d728ff2310a8f21e6a547c4c0165
carlosvigan <cfernandezgarcia@hotmail.com>
2022-04-06 19:45:52 +0100

    Mi primera prueba con Fabio

---
+++ PruebaConFabio/src/com/carlosvigan/PruebaFabio/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.carlosvigan.PruebaFabio;
+
+/**
+ * @author cfern
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola");
+	}
+
+}
