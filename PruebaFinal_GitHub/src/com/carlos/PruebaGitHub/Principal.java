8278198b5b769d06177630f7534818f17f4ada9b
carlosvigan <cfernandezgarcia@hotmail.com>
2022-04-06 12:10:09 +0100

    Versión inicial programa prueba.

---
+++ PruebaFinal_GitHub/src/com/carlos/PruebaGitHub/Principal.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.carlos.PruebaGitHub;
+
+/**
+ * @author cfern
+ *
+ */
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+	}
+
+}
---
+++ PruebaFinal_GitHub/src/module-info.java
@@ -0,0 +1,2 @@
+module PruebaFinal_GitHub {
+}
